7071ca823443e539ccfde698dc292a0f36036d8a
ljanuzi <ljanuzi@citycollege.sheffield.eu>
2021-05-28 19:25:36 +0200

    test

--- src/main/java/com/example/covid19/Covid19Application.java
+++ src/main/java/com/example/covid19/Covid19Application.java
@@ -11,13 +11,6 @@ import java.util.ArrayList;
 @Configuration
 @SpringBootApplication
 public class Covid19Application {
-    DB_Manager dbManager;
-    @Autowired
-    public Covid19Application(DB_Manager dbManager) throws SQLException {
-        this.dbManager = dbManager;
-
-        System.out.println(dbManager.medianOfData("2020-12-12","2020-12-19"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Covid19Application.class, args);
